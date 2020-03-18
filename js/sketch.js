d28f24b47cde40c18cc38e1840db10fde237e827
maya-art <60931149+maya-art@users.noreply.github.com>
2020-03-18 16:29:01 +0100

    Sound

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,32 @@
 let hue = 0;
+let osc, freq, amp;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0, 255, 0);
+    background(1000, 1000, 1000);
     colorMode(HSB, 255);
+    osc = new p5.Oscillator('triangle');
+
+    function setup() {
+        smooth();
+        createCanvas(100, 100, WEBGL);
+        // Make a <p> element and put it behind the canvas
+        let p = createP('I am a dom element');
+        p.center();
+        p.style('font-size', '20px');
+        p.style('text-align', 'center');
+        p.style('z-index', '-9999');
+    }
+
+    function draw() {
+
+        sphere(30);
+        erase();
+        rotateY(frameCount * 0.02);
+        translate(0, 0, 40);
+        torus(15, 5);
+        noErase();
+    }
 }
 
 function draw() {
@@ -12,11 +35,29 @@ function draw() {
     stroke(hue, 255, 255);
     strokeWeight(10);
     if (mouseIsPressed) {
+        osc.freq(freq, 0.1);
+        osc.amp(amp, 0.1);
         line(mouseX, mouseY, pmouseX, pmouseY);
+
     }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
+}
+
+function mousePressed() {
+    osc.start();
+    stroke(random(0, 255), random(0, 255), random(0, 255));
+}
+
+function keyPressed() {
+    if (keyCode === BACKSPACE) {
+        background(0);
+    }
+}
+
+function mouseReleased() {
+    osc.stop();
 }
