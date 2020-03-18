7f5a1fb7087676dfb800ab4f6d4c5b5a11e16e80
Simon Pinkas <simon@code-think.com>
2020-03-18 14:46:48 +0100

    Fix broken code

--- js/sketch.js
+++ js/sketch.js
@@ -1,52 +1,22 @@
+let hue = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0, 255, 0);
-    frameRate(560);
-    noStroke();
-    var draw = function() {
-        if (mouseIsPressed) {
-            ellipse(mouseX, mouseY, 5, 5);
-        }
-    };
-}
-
-//variable
-let painting = false;
-
-function startPosition() {
-    painting = true;
-}
-
-function finishedPosition() {
-    painting = false;
-    ctx.beginPath();
-}
-
-function draw(e) {
-    if (!painting) return;
-    ctx.lineWidth = 10;
-    ctx.lienCap = “round ";
-    ctx.stroke.Style = "red";
-
-
-
-    ctx..moveTo(e.clientX, e.clientY);
-
-
-}
-
-if (mouseIsPressed) {
-    rect(mouseX, mouseY, 25, 25);
+    colorMode(HSB, 255);
 }
 
 function draw() {
+    hue = map(mouseX, 0, width, 0, 255);
     fill(255);
-    rect(mouseX, mouseY, 25, 25);
+    stroke(hue, 255, 255);
+    strokeWeight(10);
+    if (mouseIsPressed) {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
 }
-
-//variable
